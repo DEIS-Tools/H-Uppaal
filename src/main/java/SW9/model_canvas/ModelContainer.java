938091370d1bdf60aa59487650d2b06c125e773b
Rasmus Holm Jensen <r.holm.j93@gmail.com>
2016-09-28 13:43:40 +0200

    Add ModelContainer

---
+++ src/main/java/SW9/model_canvas/ModelContainer.java
@@ -0,0 +1,55 @@
+package SW9.model_canvas;
+
+import SW9.MouseTracker;
+import SW9.model_canvas.edges.Edge;
+import SW9.model_canvas.locations.Location;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ModelContainer extends Parent {
+
+    private final List<Location> locations = new ArrayList<>();
+    private final List<Edge> edges = new ArrayList<>();
+    protected final MouseTracker mouseTracker = new MouseTracker(this);
+
+    public ModelContainer() {
+        super();
+
+        mouseTracker.registerOnMouseEnteredEventHandler(event -> {
+            ModelCanvas.setHoveredModelContainer(this);
+        });
+
+        mouseTracker.registerOnMouseExitedEventHandler(event -> {
+            if (this.equals(ModelCanvas.getHoveredModelContainer())) ModelCanvas.setHoveredModelContainer(null);
+        });
+    }
+
+    public List<Location> getLocations() {
+        return locations;
+    }
+
+    public boolean addLocation(final Location location) {
+        addChild(location);
+        return locations.add(location);
+    }
+
+    public boolean removeLocation(final Location location) {
+        removeChild(location);
+        return locations.remove(location);
+    }
+
+    public List<Edge> getEdges() {
+        return edges;
+    }
+
+    public boolean addEdge(final Edge edge) {
+        addChild(edge);
+        return edges.add(edge);
+    }
+
+    public boolean removeEdge(final Edge edge) {
+        removeChild(edge);
+        return edges.remove(edge);
+    }
+}
