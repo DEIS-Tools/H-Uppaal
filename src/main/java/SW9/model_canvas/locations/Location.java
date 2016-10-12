8fbbafb372079237ffee03f9964f1f01b7977d4c
feupeu <nkmouritzsen@gmail.com>
2016-10-12 14:12:50 +0200

    Add properties for locations

--- src/main/java/SW9/model_canvas/locations/Location.java
+++ src/main/java/SW9/model_canvas/locations/Location.java
@@ -3,6 +3,7 @@ package SW9.model_canvas.locations;
 import SW9.issues.Warning;
 import SW9.model_canvas.*;
 import SW9.model_canvas.edges.Edge;
+import SW9.model_canvas.edges.Properties;
 import SW9.utility.UndoRedoStack;
 import SW9.utility.colors.Color;
 import SW9.utility.colors.Colorable;
@@ -42,6 +43,11 @@ public class Location extends Parent implements MouseTrackable, Removable, Color
     private final Label locationLabel;
     private InitialLocationCircle initialLocationCircle = null;
     private FinalLocationCross finalLocationCross = null;
+
+    private final Properties properties = new Properties(
+            new Properties.Entry(Properties.Type.LOCATION_NAME, nameProperty),
+            new Properties.Entry(Properties.Type.LOCATION_INVARIANT, invariantProperty)
+    );
 
 
     // Used to create the Location
@@ -123,6 +129,13 @@ public class Location extends Parent implements MouseTrackable, Removable, Color
                 timeline.pause();
             }
         });
+    }
+
+    private void initializePropertiesBox() {
+        // Add the properties box, and bind it to the circle
+        properties.xProperty().bind(circle.centerXProperty().add(Location.RADIUS));
+        properties.yProperty().bind(circle.centerYProperty().add(Location.RADIUS));
+        addChild(properties);
     }
 
     public Location(final MouseTracker canvasMouseTracker) {
@@ -212,6 +225,9 @@ public class Location extends Parent implements MouseTrackable, Removable, Color
         // Place the new location when the mouse is pressed (i.e. stop moving it)
         canvasMouseTracker.registerOnMousePressedEventHandler(event -> {
             if (isOnMouse.get() && ModelCanvas.mouseIsHoveringModelContainer()) {
+                // Add properties box
+                initializePropertiesBox();
+
                 // Consume the event
                 event.consume();
 
@@ -289,6 +305,11 @@ public class Location extends Parent implements MouseTrackable, Removable, Color
 
         // Will make the location marked whenever it is unreachable
         initializeReachabilityEffect();
+
+        // Add the properties box, if this location is either initial of exit
+        if (type.equals(Type.INITIAL) || type.equals(Type.FINAL)) {
+            initializePropertiesBox();
+        }
     }
 
     private void makeDraggable() {
