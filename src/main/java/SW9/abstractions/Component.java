7e10b00cd92dd5fb17278581568e576663449d65
Rasmus Holm Jensen <r.holm.j93@gmail.com>
2016-10-21 10:52:40 +0200

    Add Component abstraction.

---
+++ src/main/java/SW9/abstractions/Component.java
@@ -0,0 +1,135 @@
+package SW9.abstractions;
+
+import SW9.utility.colors.Color;
+import javafx.beans.property.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class Component {
+
+    // Verification properties
+    private final StringProperty name;
+    private final StringProperty declarations = new SimpleStringProperty("");
+    private final ObservableList<Location> locations = FXCollections.observableArrayList();
+    private final ObservableList<Edge> edges = FXCollections.observableArrayList();
+
+    // Styling properties
+    private final DoubleProperty x = new SimpleDoubleProperty(0d);
+    private final DoubleProperty y = new SimpleDoubleProperty(0d);
+    private final DoubleProperty width = new SimpleDoubleProperty(0d);
+    private final DoubleProperty height = new SimpleDoubleProperty(0d);
+    private final ObjectProperty<Color> color = new SimpleObjectProperty<>(Color.GREY_BLUE);
+    private final ObjectProperty<Color.Intensity> colorIntensity = new SimpleObjectProperty<>(Color.Intensity.I700);
+
+    public Component(final String name) {
+        this(new SimpleStringProperty(name));
+    }
+
+    public Component(final StringProperty name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public void setName(final String name) {
+        this.name.set(name);
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public String getDeclarations() {
+        return declarations.get();
+    }
+
+    public void setDeclarations(final String declarations) {
+        this.declarations.set(declarations);
+    }
+
+    public StringProperty declarationsProperty() {
+        return declarations;
+    }
+
+    public ObservableList<Location> getLocations() {
+        return locations;
+    }
+
+    public ObservableList<Edge> getEdges() {
+        return edges;
+    }
+
+    public double getX() {
+        return x.get();
+    }
+
+    public void setX(final double x) {
+        this.x.set(x);
+    }
+
+    public DoubleProperty xProperty() {
+        return x;
+    }
+
+    public double getY() {
+        return y.get();
+    }
+
+    public void setY(final double y) {
+        this.y.set(y);
+    }
+
+    public DoubleProperty yProperty() {
+        return y;
+    }
+
+    public double getWidth() {
+        return width.get();
+    }
+
+    public void setWidth(final double width) {
+        this.width.set(width);
+    }
+
+    public DoubleProperty widthProperty() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height.get();
+    }
+
+    public void setHeight(final double height) {
+        this.height.set(height);
+    }
+
+    public DoubleProperty heightProperty() {
+        return height;
+    }
+
+    public Color getColor() {
+        return color.get();
+    }
+
+    public void setColor(final Color color) {
+        this.color.set(color);
+    }
+
+    public ObjectProperty<Color> colorProperty() {
+        return color;
+    }
+
+    public Color.Intensity getColorIntensity() {
+        return colorIntensity.get();
+    }
+
+    public void setColorIntensity(final Color.Intensity colorIntensity) {
+        this.colorIntensity.set(colorIntensity);
+    }
+
+    public ObjectProperty<Color.Intensity> colorIntensityProperty() {
+        return colorIntensity;
+    }
+}
