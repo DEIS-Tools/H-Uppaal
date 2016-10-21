2ba9f72aaab1d0ae4523e22ca9bd9b7a79b2bff5
feupeu <nkmouritzsen@gmail.com>
2016-10-21 23:42:32 +0200

    Move view-depending stuff to the view file

--- src/main/java/SW9/controllers/ComponentController.java
+++ src/main/java/SW9/controllers/ComponentController.java
@@ -1,28 +1,21 @@
 package SW9.controllers;
 
 import SW9.abstractions.Component;
-import SW9.abstractions.Location;
 import SW9.presentations.LocationPresentation;
-import SW9.utility.colors.Color;
-import SW9.utility.helpers.DragHelper;
 import com.jfoenix.controls.JFXButton;
 import javafx.animation.Interpolator;
 import javafx.animation.Transition;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.Initializable;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Cursor;
 import javafx.scene.Node;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Path;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
-import org.kordamp.ikonli.javafx.FontIcon;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -32,7 +25,6 @@ public class ComponentController implements Initializable {
     private final ObjectProperty<Component> component = new SimpleObjectProperty<>(null);
 
     public BorderPane toolbar;
-    public StackPane container;
     public Rectangle background;
     public TextArea declaration;
     public JFXButton toggleDeclarationButton;
@@ -43,69 +35,7 @@ public class ComponentController implements Initializable {
 
     @Override
     public void initialize(final URL location, final ResourceBundle resources) {
-        component.addListener((observable, oldComponent, newComponent) -> {
-            initializeToolbar();
 
-            // Whenever the initial location updates in the model, also update it in the view
-            component.get().initialLocationProperty().addListener((observable1, oldInitialLocation, newInitialLocation) -> {
-                initializeInitialLocation(newInitialLocation);
-            });
-
-            // Whenever the initial location updates in the model, also update it in the view
-            component.get().finalLocationProperty().addListener((observable1, oldFinalLocation, newFinalLocation) -> {
-                initializeFinalLocation(newFinalLocation);
-            });
-
-            initializeInitialLocation(newComponent.getInitialLocation());
-            initializeFinalLocation(newComponent.getFinalLocation());
-        });
-
-        initializeFrame();
-
-    }
-
-    private void initializeInitialLocation(final Location newInitialLocation) {
-        initialLocation.setLocation(newInitialLocation);
-
-        StackPane.setAlignment(initialLocation, Pos.TOP_LEFT);
-    }
-
-    private void initializeFinalLocation(final Location newFinalLocation) {
-        finalLocation.setLocation(newFinalLocation);
-
-        StackPane.setAlignment(finalLocation, Pos.BOTTOM_RIGHT);
-    }
-
-    private void initializeToolbar() {
-        final Color color = getComponent().getColor();
-        final Color.Intensity colorIntensity = getComponent().getColorIntensity();
-
-        // Set the background of the toolbar
-        toolbar.setBackground(new Background(new BackgroundFill(
-                color.getColor(colorIntensity),
-                CornerRadii.EMPTY,
-                Insets.EMPTY
-        )));
-
-        // Set the border of the toolbar
-        toolbar.setBorder(new Border(new BorderStroke(
-                color.getColor(colorIntensity.next(2)),
-                BorderStrokeStyle.SOLID,
-                CornerRadii.EMPTY,
-                new BorderWidths(1)
-        )));
-
-        // Set the icon color and rippler color of the toggleDeclarationButton
-        toggleDeclarationButton.setRipplerFill(color.getTextColor(colorIntensity));
-        ((FontIcon) toggleDeclarationButton.getGraphic()).setFill(color.getTextColor(colorIntensity));
-
-        // Set a hover effect for the toggleDeclarationButton
-        toggleDeclarationButton.setOnMouseEntered(event -> toggleDeclarationButton.setCursor(Cursor.HAND));
-        toggleDeclarationButton.setOnMouseExited(event -> toggleDeclarationButton.setCursor(Cursor.DEFAULT));
-    }
-
-    private void initializeFrame() {
-        // TODO: Crop corners
     }
 
     public void toggleDeclaration(final MouseEvent mouseEvent) {
--- src/main/java/SW9/presentations/ComponentPresentation.java
+++ src/main/java/SW9/presentations/ComponentPresentation.java
@@ -1,16 +1,24 @@
 package SW9.presentations;
 
 import SW9.abstractions.Component;
+import SW9.abstractions.Location;
 import SW9.controllers.ComponentController;
 import SW9.utility.colors.Color;
 import SW9.utility.helpers.DragHelper;
 import SW9.utility.helpers.MouseTrackable;
 import SW9.utility.mouse.MouseTracker;
 import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.JavaFXBuilderFactory;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Cursor;
 import javafx.scene.effect.BlendMode;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
+import javafx.scene.shape.*;
+import org.kordamp.ikonli.javafx.FontIcon;
 
 import java.io.IOException;
 import java.net.URL;
@@ -18,8 +26,10 @@ import java.util.Random;
 
 public class ComponentPresentation extends StackPane implements MouseTrackable {
 
+    private final static double CORNER_SIZE = 50;
+
     private final ComponentController controller;
-    private final Component component;
+    private final ObjectProperty<Component> component = new SimpleObjectProperty<>();
 
     private final MouseTracker mouseTracker = new MouseTracker(this);
 
@@ -35,12 +45,18 @@ public class ComponentPresentation extends StackPane implements MouseTrackable {
         fxmlLoader.setBuilderFactory(new JavaFXBuilderFactory());
 
         try {
+            initializeToolbar();
+            initializeFrame();
+            initializeInitialLocation();
+            initializeFinalLocation();
+            initializeBackground();
+
             fxmlLoader.setRoot(this);
             fxmlLoader.load(location.openStream());
 
             controller = fxmlLoader.getController();
-            this.component = component;
-            controller.setComponent(this.component);
+            controller.setComponent(component);
+            this.component.bind(controller.componentProperty());
 
             // Find the x and y coordinates to the values in the model
             layoutXProperty().bind(component.xProperty());
@@ -52,8 +68,6 @@ public class ComponentPresentation extends StackPane implements MouseTrackable {
             minHeightProperty().bind(component.heightProperty());
             maxHeightProperty().bind(component.heightProperty());
 
-            initializeBackground();
-
             DragHelper.makeDraggable(this);
 
         } catch (final IOException ioe) {
@@ -61,29 +75,115 @@ public class ComponentPresentation extends StackPane implements MouseTrackable {
         }
     }
 
+    private void initializeInitialLocation() {
+        component.addListener((observable, oldValue, component) -> {
+            controller.initialLocation.setLocation(component.getInitialLocation());
+            controller.initialLocation.setTranslateX(7);
+            controller.initialLocation.setTranslateY(7);
+            controller.initialLocation.toFront();
+
+            StackPane.setAlignment(controller.initialLocation, Pos.TOP_LEFT);
+        });
+    }
+
+    private void initializeFinalLocation() {
+        component.addListener((observable, oldValue, component) -> {
+            controller.finalLocation.setLocation(component.getFinalLocation());
+            controller.finalLocation.setTranslateX(-7);
+            controller.finalLocation.setTranslateY(-7);
+            controller.finalLocation.toFront();
+
+            StackPane.setAlignment(controller.finalLocation, Pos.BOTTOM_RIGHT);
+        });
+    }
+
+    private void initializeToolbar() {
+        component.addListener((observable, oldValue, component) -> {
+            final Color color = component.getColor();
+            final Color.Intensity colorIntensity = component.getColorIntensity();
+
+            // Set the background of the toolbar
+            controller.toolbar.setBackground(new Background(new BackgroundFill(
+                    color.getColor(colorIntensity),
+                    CornerRadii.EMPTY,
+                    Insets.EMPTY
+            )));
+
+            // Set the icon color and rippler color of the toggleDeclarationButton
+            controller.toggleDeclarationButton.setRipplerFill(color.getTextColor(colorIntensity));
+            ((FontIcon) controller.toggleDeclarationButton.getGraphic()).setFill(color.getTextColor(colorIntensity));
+
+            // Set a hover effect for the controller.toggleDeclarationButton
+            controller.toggleDeclarationButton.setOnMouseEntered(event -> controller.toggleDeclarationButton.setCursor(Cursor.HAND));
+            controller.toggleDeclarationButton.setOnMouseExited(event -> controller.toggleDeclarationButton.setCursor(Cursor.DEFAULT));
+        });
+    }
+
+    private void initializeFrame() {
+        component.addListener((observable, oldValue, component) -> {
+            Shape mask;
+            final Rectangle rectangle = new Rectangle(component.getWidth(), component.getHeight());
+
+            // Generate first corner (to subtract)
+            final Polygon corner1 = new Polygon(
+                    0, 0,
+                    CORNER_SIZE + 2, 0,
+                    0, CORNER_SIZE + 2
+            );
+
+            // Generate second corner (to subtract)
+            final Polygon corner2 = new Polygon(
+                    component.getWidth(), component.getHeight(),
+                    component.getWidth() - CORNER_SIZE - 2, component.getHeight(),
+                    component.getWidth(), component.getHeight() - CORNER_SIZE - 2
+            );
+
+            // Mask the parent of the frame (will also mask the background)
+            mask = Path.subtract(rectangle, corner1);
+            mask = Path.subtract(mask, corner2);
+            controller.frame.getParent().setClip(mask);
+
+            // Add the missing lines that we cropped away
+            final Line line1 = new Line(CORNER_SIZE - 1, 0, 0, CORNER_SIZE - 1);
+            line1.setFill(component.getColor().getColor(component.getColorIntensity().next(2)));
+            line1.setStrokeWidth(2);
+            StackPane.setAlignment(line1, Pos.TOP_LEFT);
+            getChildren().add(line1);
+
+            final Line line2 = new Line(CORNER_SIZE, 0, 0, CORNER_SIZE);
+            line2.setFill(component.getColor().getColor(component.getColorIntensity().next(2)));
+            line2.setStrokeWidth(2);
+            StackPane.setAlignment(line2, Pos.BOTTOM_RIGHT);
+            getChildren().add(line2);
+        });
+    }
+
     private void initializeBackground() {
-        // Bind the background width and height to the values in the model
-        controller.background.widthProperty().bind(component.widthProperty());
-        controller.background.heightProperty().bind(component.heightProperty());
+        component.addListener((observable, oldValue, component) -> {
+            // Bind the background width and height to the values in the model
+            controller.background.widthProperty().bind(component.widthProperty());
+            controller.background.heightProperty().bind(component.heightProperty());
 
-        // Set the background color to the lightest possible version of the color
-        controller.background.setFill(component.getColor().getColor(Color.Intensity.I50));
+            // Set the background color to the lightest possible version of the color
+            controller.background.setFill(Color.AMBER.getColor(Color.Intensity.I500));
 
-        // Set the stroke color to two shades darker
-        controller.background.setStroke(component.getColor().getColor(component.getColorIntensity().next(2)));
+            // Set the stroke color to two shades darker
+            controller.background.setStrokeWidth(2);
+            controller.background.setStroke(component.getColor().getColor(component.getColorIntensity().next(2)));
 
-        // Will let the grid show through
-        controller.background.blendModeProperty().set(BlendMode.MULTIPLY);
+            // Will let the grid show through
+            controller.background.blendModeProperty().set(BlendMode.MULTIPLY);
+        });
     }
 
     @Override
     public DoubleProperty xProperty() {
-        return component.xProperty();
+        return component.get().xProperty();
     }
 
     @Override
     public DoubleProperty yProperty() {
-        return component.yProperty();
+        return component.get().yProperty();
     }
 
     @Override
