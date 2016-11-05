57fad51188de1efc7f6b9d656de661c05fe63646
feupeu <nkmouritzsen@gmail.com>
2016-11-05 02:18:17 +0100

    Make run all queries and clear all queries buttons circular

--- src/main/java/SW9/controllers/QueryPaneController.java
+++ src/main/java/SW9/controllers/QueryPaneController.java
@@ -6,6 +6,7 @@ import SW9.abstractions.QueryState;
 import SW9.presentations.QueryPresentation;
 import SW9.utility.helpers.DropShadowHelper;
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXRippler;
 import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -23,13 +24,14 @@ public class QueryPaneController implements Initializable {
     public Label toolbarTitle;
     public JFXButton addQueryButton;
     public AnchorPane toolbar;
-    public JFXButton runAllQueriesButton;
-    public JFXButton clearAllQueriesButton;
+    public JFXRippler runAllQueriesButton;
+    public JFXRippler clearAllQueriesButton;
     public VBox queriesList;
     public StackPane root;
 
     @Override
     public void initialize(final URL location, final ResourceBundle resources) {
+
         // We need to register these event manually this way because JFXButton overrides onPressed and onRelease to handle rippler effect
         addQueryButton.addEventHandler(MouseEvent.MOUSE_PRESSED, event -> addQueryButtonPressed());
         addQueryButton.addEventHandler(MouseEvent.MOUSE_RELEASED, event -> addQueryButtonReleased());
--- src/main/java/SW9/presentations/QueryPanePresentation.java
+++ src/main/java/SW9/presentations/QueryPanePresentation.java
@@ -3,6 +3,7 @@ package SW9.presentations;
 import SW9.controllers.QueryPaneController;
 import SW9.utility.colors.Color;
 import SW9.utility.helpers.DropShadowHelper;
+import com.jfoenix.controls.JFXRippler;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.JavaFXBuilderFactory;
 import javafx.geometry.Insets;
@@ -52,20 +53,17 @@ public class QueryPanePresentation extends StackPane {
 
         // Set the font color of elements in the toolbar
         controller.toolbarTitle.setTextFill(color.getTextColor(colorIntensity));
-        controller.runAllQueriesButton.setRipplerFill(color.getTextColor(colorIntensity));
-        controller.clearAllQueriesButton.setRipplerFill(color.getTextColor(colorIntensity));
-        ((FontIcon) controller.runAllQueriesButton.getGraphic()).setFill(color.getTextColor(colorIntensity));
-        ((FontIcon) controller.clearAllQueriesButton.getGraphic()).setFill(color.getTextColor(colorIntensity));
 
         controller.runAllQueriesButton.setBackground(new Background(new BackgroundFill(
                 javafx.scene.paint.Color.TRANSPARENT,
                 new CornerRadii(100),
                 Insets.EMPTY)));
 
-        controller.clearAllQueriesButton.setBackground(new Background(new BackgroundFill(
-                javafx.scene.paint.Color.TRANSPARENT,
-                new CornerRadii(100),
-                Insets.EMPTY)));
+        controller.runAllQueriesButton.setMaskType(JFXRippler.RipplerMask.CIRCLE);
+        controller.runAllQueriesButton.setRipplerFill(color.getTextColor(colorIntensity));
+
+        controller.clearAllQueriesButton.setMaskType(JFXRippler.RipplerMask.CIRCLE);
+        controller.clearAllQueriesButton.setRipplerFill(color.getTextColor(colorIntensity));
 
         // Set the elevation of the toolbar
         controller.toolbar.setEffect(DropShadowHelper.generateElevationShadow(8));
