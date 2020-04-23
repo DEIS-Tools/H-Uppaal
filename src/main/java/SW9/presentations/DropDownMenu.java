9431e8be7dc981fd6aec3886ab3af89e7b63dda1
Kenneth Yrke Jørgensen <kenneth@yrke.dk>
2020-04-23 09:55:36 +0200

    Comment out code not wokring due to api changed

--- src/main/java/SW9/presentations/DropDownMenu.java
+++ src/main/java/SW9/presentations/DropDownMenu.java
@@ -76,14 +76,22 @@ public class DropDownMenu {
         list.setOnMouseExited(event -> isHoveringMenu.set(false));
         list.setOnMouseEntered(event -> isHoveringMenu.set(true));
 
+        //XXX: removed due to api changed
+        /*
+        popup.setContent(content);
+        popup.setPopupContainer(container);
+        popup.setSource(source);
+        */
     }
 
     public void close() {
-
+        //XXX: removed due to api changed
+        //popup.close();
     }
 
     public void show(final JFXPopup.PopupVPosition vAlign, final JFXPopup.PopupHPosition hAlign, final double initOffsetX, final double initOffsetY) {
-
+        //XXX: removed due to api changed
+        popup.show(vAlign, hAlign, initOffsetX, initOffsetY);
     }
 
     public void addListElement(final String s) {
