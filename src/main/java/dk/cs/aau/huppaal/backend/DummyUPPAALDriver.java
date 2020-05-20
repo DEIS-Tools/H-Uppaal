82744cd391184b4a73ec0ed7aa648bce7d91a753
Niels Vistisen <nielswps@gmail.com>
2020-05-20 21:04:46 +0200

    DummyUPPAALDriver toasts added

--- src/main/java/dk/cs/aau/huppaal/backend/DummyUPPAALDriver.java
+++ src/main/java/dk/cs/aau/huppaal/backend/DummyUPPAALDriver.java
@@ -1,6 +1,7 @@
 package dk.cs.aau.huppaal.backend;
 
 import com.uppaal.engine.Engine;
+import dk.cs.aau.huppaal.HUPPAAL;
 import dk.cs.aau.huppaal.abstractions.Component;
 import dk.cs.aau.huppaal.abstractions.Location;
 
@@ -20,41 +21,48 @@ public class DummyUPPAALDriver implements IUPPAALDriver {
 
     @Override
     public Thread runQuery(String query, Consumer<Boolean> success, Consumer<BackendException> failure) {
+        HUPPAAL.showToast("UPPAAL not found");
         return null;
     }
 
     @Override
     public Thread runQuery(String query, Consumer<Boolean> success, Consumer<BackendException> failure, long timeout) {
+        HUPPAAL.showToast("UPPAAL not found");
         return null;
     }
 
     @Override
     public Thread runQuery(String query, Consumer<Boolean> success, Consumer<BackendException> failure, Consumer<Engine> engineConsumer) {
+        HUPPAAL.showToast("UPPAAL not found");
         return null;
     }
 
     @Override
     public Thread runQuery(String query, Consumer<Boolean> success, Consumer<BackendException> failure, Consumer<Engine> engineConsumer, QueryListener queryListener) {
+        HUPPAAL.showToast("UPPAAL not found");
         return null;
     }
 
     @Override
     public void stopEngines() {
-
+        HUPPAAL.showToast("UPPAAL not found");
     }
 
     @Override
     public String getLocationReachableQuery(Location location, Component component) {
+        HUPPAAL.showToast("UPPAAL not found");
         return null;
     }
 
     @Override
     public String getExistDeadlockQuery(Component component) {
+        HUPPAAL.showToast("UPPAAL not found");
         return null;
     }
 
     @Override
     public File getServerFile() {
+        HUPPAAL.showToast("UPPAAL not found");
         return null;
     }
 }
