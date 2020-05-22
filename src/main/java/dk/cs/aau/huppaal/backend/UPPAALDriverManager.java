d51862473d690f1f8c933c7b2d1de7f50ddf2a19
Niels Vistisen <nielswps@gmail.com>
2020-05-22 20:10:52 +0200

    WIP: Todos added

--- src/main/java/dk/cs/aau/huppaal/backend/UPPAALDriverManager.java
+++ src/main/java/dk/cs/aau/huppaal/backend/UPPAALDriverManager.java
@@ -7,9 +7,15 @@ public final class UPPAALDriverManager {
 
     public static synchronized IUPPAALDriver getInstance(){
         if(instance == null){
+            //Todo: find better way to check if the server file exists
             instance = new UPPAALDriver();
             if(!instance.getServerFile().exists()){
                 instance = new DummyUPPAALDriver();
+            } else {
+                //Todo: handle checks set to local only (possibly by adding a variable that can be set)
+                if(false){
+                    instance = new LocalUPPAALDriver();
+                }
             }
         }
 
