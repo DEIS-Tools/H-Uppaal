c89808b20c9336dabce2cf494c9fa7805562be93
Niels Vistisen <nielswps@gmail.com>
2020-07-08 10:37:46 +0200

    WIP: Handle grid on drag

--- src/main/java/dk/cs/aau/huppaal/presentations/CanvasPresentation.java
+++ src/main/java/dk/cs/aau/huppaal/presentations/CanvasPresentation.java
@@ -1,5 +1,6 @@
 package dk.cs.aau.huppaal.presentations;
 
+import dk.cs.aau.huppaal.HUPPAAL;
 import dk.cs.aau.huppaal.controllers.CanvasController;
 import dk.cs.aau.huppaal.utility.UndoRedoStack;
 import dk.cs.aau.huppaal.utility.helpers.CanvasDragHelper;
@@ -15,11 +16,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.JavaFXBuilderFactory;
 import javafx.scene.Parent;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyCodeCombination;
-import javafx.scene.input.KeyCombination;
-import javafx.scene.input.MouseButton;
+import javafx.scene.input.*;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
 import javafx.scene.shape.Line;
 
 import java.io.IOException;
@@ -75,7 +77,6 @@ public class CanvasPresentation extends Pane implements MouseTrackable {
             controller.component.setLayoutY(GRID_SIZE / 2);
 
             */
-
             CanvasDragHelper.makeDraggable(this, mouseEvent -> mouseEvent.getButton().equals(MouseButton.SECONDARY));
         } catch (final IOException ioe) {
             throw new IllegalStateException(ioe);
@@ -84,6 +85,19 @@ public class CanvasPresentation extends Pane implements MouseTrackable {
 
     private void initializeGrid() {
         final Grid grid = new Grid(GRID_SIZE);
+
+        this.addEventFilter(MouseEvent.MOUSE_CLICKED, (mouseEvent) -> {
+            if(mouseEvent.getButton().equals(MouseButton.SECONDARY)){
+                grid.setInitialDragPosition(mouseEvent.getX(), mouseEvent.getY());
+            }
+        });
+
+        this.addEventFilter(MouseEvent.MOUSE_DRAGGED, (mouseEvent) -> {
+            if(mouseEvent.getButton().equals(MouseButton.SECONDARY)){
+                grid.reactToDrag(mouseEvent);
+            }
+        });
+
         getChildren().add(grid);
         grid.toBack();
     }
@@ -121,6 +135,8 @@ public class CanvasPresentation extends Pane implements MouseTrackable {
 
         private final ArrayList<Line> horizontalLines = new ArrayList<>();
         private final ArrayList<Line> verticalLines = new ArrayList<>();
+        private double lastLineAddedX = 0;
+        private double lastLineAddedY = 0;
 
         public Grid(final int gridSize) {
 
@@ -200,6 +216,83 @@ public class CanvasPresentation extends Pane implements MouseTrackable {
                     horizontalLines.forEach(line -> getChildren().add(line));
                 });
             });
+        }
+
+        public void reactToDrag(MouseEvent mouseEvent) {
+            if(mouseEvent.getY() - lastLineAddedY > GRID_SIZE){
+                double lineY = horizontalLines.get(horizontalLines.size() - 1).getStartY();
+
+                final Line line = new Line(200, lineY + GRID_SIZE, 300, lineY + GRID_SIZE);
+                line.getStyleClass().add("grid-line");
+
+                final DoubleBinding parentYBinding = new DoubleBinding() {
+                    {
+                        super.bind(translateYProperty());
+                    }
+
+                    @Override
+                    protected double computeValue() {
+                        final int moveFactor = (int) (getParent().getTranslateY() / GRID_SIZE);
+                        return -1 * moveFactor * GRID_SIZE + 0.5 * GRID_SIZE;
+                    }
+                };
+
+                line.layoutYProperty().bind(parentYBinding);
+                line.startXProperty().bind(getParent().layoutXProperty().subtract(getParent().translateXProperty()).subtract(1500));
+                line.endXProperty().bind(getParent().layoutXProperty().subtract(getParent().translateXProperty()).add(getScene().widthProperty()).add(1500));
+
+                Line oldLine = horizontalLines.get(0);
+
+                horizontalLines.add(line);
+                horizontalLines.remove(0);
+
+                lastLineAddedY -= GRID_SIZE;
+
+                getChildren().remove(oldLine);
+                getChildren().add(line);
+            } else if(mouseEvent.getY() - lastLineAddedY < -GRID_SIZE) {
+                double lineY = horizontalLines.get(0).getStartY();
+
+                final Line line = new Line(200, lineY - GRID_SIZE, 300, lineY - GRID_SIZE);
+                line.getStyleClass().add("grid-line");
+                line.setStroke(new LinearGradient(0d, -5d, 0d, 5d, false,
+                        CycleMethod.NO_CYCLE, new Stop(0, Color.BLACK),
+                        new Stop(0.199,Color.BLACK),
+                        new Stop(0.2,Color.RED),
+                        new Stop(0.799,Color.RED),
+                        new Stop(0.8,Color.BLACK)));
+
+                final DoubleBinding parentYBinding = new DoubleBinding() {
+                    {
+                        super.bind(translateYProperty());
+                    }
+
+                    @Override
+                    protected double computeValue() {
+                        final int moveFactor = (int) (getParent().getTranslateY() / GRID_SIZE);
+                        return -1 * moveFactor * GRID_SIZE + 0.5 * GRID_SIZE;
+                    }
+                };
+
+                line.layoutYProperty().bind(parentYBinding);
+                line.startXProperty().bind(getParent().layoutXProperty().subtract(getParent().translateXProperty()).subtract(1500));
+                line.endXProperty().bind(getParent().layoutXProperty().subtract(getParent().translateXProperty()).add(getScene().widthProperty()).add(1500));
+
+                Line oldLine = horizontalLines.get(horizontalLines.size() - 1);
+
+                horizontalLines.add(0, line);
+                horizontalLines.remove(horizontalLines.size() - 1);
+
+                lastLineAddedY += GRID_SIZE;
+
+                getChildren().remove(oldLine);
+                getChildren().add(line);
+            }
+        }
+
+        public void setInitialDragPosition(double x, double y) {
+            lastLineAddedX = x;
+            lastLineAddedY = y;
         }
     }
 }
