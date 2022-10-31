a2b3c9c4872fc881c8b6a119fe9f8e8dc41d41a2
Asger Gitz-Johansen <agj@hmkbilcon.dk>
2022-10-31 08:57:46 +0100

    fix: capitalize greeting

--- src/main/java/HUPPAAL.java
+++ src/main/java/HUPPAAL.java
@@ -3,7 +3,7 @@ import dk.cs.aau.huppaal.BuildConfig;
 public class HUPPAAL {
     //For JavaFX Runtime Components are Missing https://edencoding.com/runtime-components-error/ - 2021-02-09
 	public static void main(String[] args) {
-        System.out.printf("welcome to %s version v%s+%s%n", BuildConfig.NAME, BuildConfig.VERSION, BuildConfig.COMMIT_SHA_SHORT);
+        System.out.printf("Welcome to %s version v%s+%s%n", BuildConfig.NAME, BuildConfig.VERSION, BuildConfig.COMMIT_SHA_SHORT);
         dk.cs.aau.huppaal.HUPPAAL.main(args);
     }
 }
